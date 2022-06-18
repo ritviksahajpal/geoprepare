fd332e494122814feffebdb8e674a06c37ea376d
Ritvik Sahajpal <ritvik@umd.edu>
2022-06-18 17:40:54 -0400

    NDVI, AgERA5

--- geoprepare/datasets/NDVI.py
+++ geoprepare/datasets/NDVI.py
@@ -161,112 +161,112 @@ def run(params):
 
     ## validate arguments
     if params.scale_glam:
-        assert not params.scale_mark, "Set exactly one of -scale_glam or -scale_mark!"
+       assert not params.scale_mark, "Set exactly one of -scale_glam or -scale_mark!"
     else:
-        assert params.scale_mark, "Set exactly one of -scale_glam or -scale_mark!"
-        assert params.vi == 'ndvi', "MARK scaling should only be used with NDVI"
+       assert params.scale_mark, "Set exactly one of -scale_glam or -scale_mark!"
+       assert params.vi == 'ndvi', "MARK scaling should only be used with NDVI"
 
-	## clean and collect existing files
-	# remove running composites
-	runningComposites = glob.glob(os.path.join(params.dir_interim,"*.running_composite.tif"))
-	for c in runningComposites:
-		os.remove(c)
-	# get list of existing full files
-	extantFiles = glob.glob(os.path.join(params.dir_interim,f"*.tif"))
+    ## clean and collect existing files
+    # remove running composites
+    runningComposites = glob.glob(os.path.join(params.dir_interim,"*.running_composite.tif"))
+    for c in runningComposites:
+        os.remove(c)
+    # get list of existing full files
+    extantFiles = glob.glob(os.path.join(params.dir_interim,f"*.tif"))
 
-	## get missing dates
-	# first and last year
-	if params.start_year:
-		firstYear = int(params.start_year)
-	else:
-		firstYear = int(datetime.now().strftime("%Y"))
-		for f in extantFiles:
-			try:
-				fileYear = int(os.path.basename(f).split(".")[4])
-				if fileYear < firstYear:
-					firstYear = fileYear
-			except ValueError:
-				continue
-	# range of years and doys
-	years = [y for y in range(firstYear,int(datetime.now().strftime("%Y"))+1)]
-	doys =[ d for d in range(1,365,8)]
+    ## get missing dates
+    # first and last year
+    if params.start_year:
+        firstYear = int(params.start_year)
+    else:
+        firstYear = int(datetime.now().strftime("%Y"))
+        for f in extantFiles:
+            try:
+                fileYear = int(os.path.basename(f).split(".")[4])
+                if fileYear < firstYear:
+                    firstYear = fileYear
+            except ValueError:
+                continue
+    # range of years and doys
+    years = [y for y in range(firstYear,int(datetime.now().strftime("%Y"))+1)]
+    doys =[ d for d in range(1,365,8)]
 
-	# filter missing dates
-	dates = {}
-	earliestDataDict = {"MOD09CMG":"2000.055","MYD09CMG":"2002.185"}
-	for y in tqdm(years, desc='year'):
-		for doy in tqdm(doys, desc='doy', leave=False):
-			if not os.path.exists(os.path.join(params.dir_interim,generateFileName(params.product,params.vi, y,doy))):
-				formattedDate = datetime.strptime(f"{y}.{doy}","%Y.%j").strftime("%Y-%m-%d")
-				# is the image in the future?
-				if datetime.strptime(f"{y}.{doy}","%Y.%j") >= datetime.now() or datetime.strptime(f"{y}.{doy}","%Y.%j") < datetime.strptime(earliestDataDict.get(params.product,"2000.049"),"%Y.%j"):
-					continue
-				# does the image exist at all?
-				if len(octvi.url.getDates(params.product,formattedDate)) == 0: # if there is valid imagery for the date
-					dates[formattedDate] = "No imagery"
-				# are there missing files in the compositing period?
-				elif not check8DayValidity(params.product,formattedDate):
-					dates[formattedDate] = "Incomplete compositing period"
-				# if none of the above, it's available.
-				else:
-					dates[formattedDate] = "Available"
-	# special behavior if "-print_missing" is set
-	if params.print_missing:
-		for k in dates.keys():
-			outString = f"{k} : {dates[k]}"
-			print(outString)
-		sys.exit()
+    # filter missing dates
+    dates = {}
+    earliestDataDict = {"MOD09CMG":"2000.055","MYD09CMG":"2002.185"}
+    for y in tqdm(years, desc='year'):
+        for doy in tqdm(doys, desc='doy', leave=False):
+            if not os.path.exists(os.path.join(params.dir_interim,generateFileName(params.product,params.vi, y,doy))):
+                formattedDate = datetime.strptime(f"{y}.{doy}","%Y.%j").strftime("%Y-%m-%d")
+                # is the image in the future?
+                if datetime.strptime(f"{y}.{doy}","%Y.%j") >= datetime.now() or datetime.strptime(f"{y}.{doy}","%Y.%j") < datetime.strptime(earliestDataDict.get(params.product,"2000.049"),"%Y.%j"):
+                    continue
+                # does the image exist at all?
+                if len(octvi.url.getDates(params.product,formattedDate)) == 0: # if there is valid imagery for the date
+                    dates[formattedDate] = "No imagery"
+                # are there missing files in the compositing period?
+                elif not check8DayValidity(params.product,formattedDate):
+                    dates[formattedDate] = "Incomplete compositing period"
+                # if none of the above, it's available.
+                else:
+                    dates[formattedDate] = "Available"
+    # special behavior if "-print_missing" is set
+    if params.print_missing:
+        for k in dates.keys():
+            outString = f"{k} : {dates[k]}"
+            print(outString)
+        sys.exit()
 
-	if params.vi == 'ndvi':
-		availableFiles = 0
-		completedFiles = 0
-		for d in dates.keys():
-			if dates[d] == "Available":
-				availableFiles += 1
-				log.info(f"Creating Composite for {d}")
-				outPath = os.path.join(params.dir_interim,generateFileName(params.product,params.vi,date=d))
-				octvi.globalVi(params.product,d,outPath)
-				if params.scale_mark:
-					scaleConversion_glamToMark(outPath)
+    if params.vi == 'ndvi':
+        availableFiles = 0
+        completedFiles = 0
+        for d in dates.keys():
+            if dates[d] == "Available":
+                availableFiles += 1
+                log.info(f"Creating Composite for {d}")
+                outPath = os.path.join(params.dir_interim,generateFileName(params.product,params.vi,date=d))
+                octvi.globalVi(params.product,d,outPath)
+                if params.scale_mark:
+                    scaleConversion_glamToMark(outPath)
 
-				## reproject to WGS 1984
-				srs = 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]'
-				ds = gdal.Open(outPath,1)
-				if ds:
-					res = ds.SetProjection(srs)
-					if res != 0:
-						log.error("--projection failed: {}".format(str(res)))
-						ds = None
-						continue
-					ds = None
-					completedFiles += 1
-				else:
-					log.error("--could not open with GDAL")
-	elif params.vi == 'gcvi':
-		availableFiles = 0
-		completedFiles = 0
-		for d in dates.keys():
-			if dates[d] == "Available":
-				availableFiles += 1
-				log.info(f"Creating Composite for {d}")
-				outPath = os.path.join(params.dir_interim, generateFileName(params.product, params.vi,date=d))
-				octvi.globalVi(params.product, d, outPath,vi="GCVI")
+                ## reproject to WGS 1984
+                srs = 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]'
+                ds = gdal.Open(outPath,1)
+                if ds:
+                    res = ds.SetProjection(srs)
+                    if res != 0:
+                        log.error("--projection failed: {}".format(str(res)))
+                        ds = None
+                        continue
+                    ds = None
+                    completedFiles += 1
+                else:
+                    log.error("--could not open with GDAL")
+    elif params.vi == 'gcvi':
+        availableFiles = 0
+        completedFiles = 0
+        for d in dates.keys():
+            if dates[d] == "Available":
+                availableFiles += 1
+                log.info(f"Creating Composite for {d}")
+                outPath = os.path.join(params.dir_interim, generateFileName(params.product, params.vi,date=d))
+                octvi.globalVi(params.product, d, outPath,vi="GCVI")
 
-				## reproject to WGS 1984
-				srs = 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]'
-				ds = gdal.Open(outPath, 1)
-				if ds:
-					res = ds.SetProjection(srs)
-					if res != 0:
-						log.error("--projection failed: {}".format(str(res)))
-						ds = None
-						continue
-					ds = None
-					completedFiles += 1
-				else:
-					log.error("--could not open with GDAL")
+                ## reproject to WGS 1984
+                srs = 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]'
+                ds = gdal.Open(outPath, 1)
+                if ds:
+                    res = ds.SetProjection(srs)
+                    if res != 0:
+                        log.error("--projection failed: {}".format(str(res)))
+                        ds = None
+                        continue
+                    ds = None
+                    completedFiles += 1
+                else:
+                    log.error("--could not open with GDAL")
 
-	log.info(f"Done. {availableFiles} composites available; {completedFiles} composites created. Use -print_missing flag to see details of missing composites.")
+    log.info(f"Done. {availableFiles} composites available; {completedFiles} composites created. Use -print_missing flag to see details of missing composites.")
 
 if __name__ == "__main__":
 	pass
