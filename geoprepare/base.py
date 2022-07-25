5be00cf1c397f3ef33c0e9bbbdf01e6f09d9ef37
Ritvik Sahajpal <ritvik@umd.edu>
2022-07-25 12:53:47 -0400

    test

--- geoprepare/base.py
+++ geoprepare/base.py
@@ -19,7 +19,6 @@ from logging import CRITICAL, ERROR, WARNING, WARN, INFO, DEBUG, NOTSET
 
 class BaseGeo:
     def __init__(self, path_config_file=['geoprepare.txt', 'geoextract.txt']):
-        breakpoint() #
         self.parser = common.read_config(path_config_file)
         self.redo_last_year = True
 
@@ -31,6 +30,24 @@ class BaseGeo:
         else:
             self.logger.info(f'Downloading {section}')
             self.logger.info(pformat(dict(self.parser[section])))
+
+    def compute_logging_level(self):
+        level = 10
+
+        if self.logging_level == 'DEBUG':
+            level = 10
+        elif self.logging_level == 'INFO':
+            level = 20
+        elif self.logging_level == 'WARNING':
+            level = 30
+        elif self.logging_level == 'ERROR':
+            level = 40
+        elif self.logging_level == 'CRITICAL':
+            level = 50
+        else:
+            raise ValueError(f'Invalid logging level {self.logging_level}')
+
+        return level
 
     def parse_config(self, section='DEFAULT'):
         """
@@ -53,17 +70,10 @@ class BaseGeo:
         self.start_year = self.parser.getint(section, 'start_year')
         self.end_year = self.parser.getint(section, 'end_year')
         self.fraction_cpus = self.parser.getfloat(section, 'fraction_cpus')
-        level_returned = 10
-        if self.logging_level=='DEBUG':
-            level_returned = 10
-        elif self.logging_level=='INFO':
-            level_returned = 20
-        elif self.logging_level=='WARNING':
-            level_returned = 30
-        elif self.logging_level=='ERROR':
-            level_returned = 40
-        elif self.logging_level =='CRITICAL':
-            level_returned = 50
+
+        level = self.compute_logging_level()
+
         # Set up logger
         self.logger = log.Logger(dir_log=self.dir_log,
-                                 name_fl=self.parser.get('DEFAULT', 'logfile'),level=level_returned)
+                                 name_fl=self.parser.get('DEFAULT', 'logfile'),
+                                 level=level)
