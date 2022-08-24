cdb21591a79cf9e9c12db5da30503f7e63a03aee
Ritvik Sahajpal <ritvik@umd.edu>
2022-08-23 23:05:58 -0400

    geomerge initial checkin

--- geoprepare/merge/merge_all.py
+++ geoprepare/merge/merge_all.py
@@ -1,6 +1,7 @@
 import os
 import ast
 
+import itertools
 from tqdm import tqdm
 from pathlib import Path
 from multiprocessing import Pool, cpu_count
@@ -10,52 +11,64 @@ import pandas as pd
 from .. import common
 
 
-def run(params):
-    num_cpus = int(params.fraction_cpus * cpu_count()) if params.parallel_process else 1
-    years = list(range(params.start_year, params.end_year + 1))
-    cols = ['country', 'region', 'region_id', 'year', 'doy']
+def create_run_combinations(params):
+    all_combinations = []
 
-    pbar = tqdm(params.countries)
-    for country in pbar:
-        pbar.set_description(f'{country}')
-        pbar.update()
-
-        # Check if we use a cropland mask or not
-        use_cropland_mask = params.parser.get(country, 'use_cropland_mask')
+    for country in params.countries:
         crops = ast.literal_eval(params.parser.get(country, 'crops'))
-        vars = ast.literal_eval(params.parser.get(country, 'eo_model'))
-        scale = ast.literal_eval(params.parser.get(country, 'scale'))
-        season = ast.literal_eval(params.parser.get(country, 'season'))
-        threshold = params.parser.getboolean(country, 'threshold')
-        limit = common.crop_mask_limit(params, country, threshold)
+        scales = ast.literal_eval(params.parser.get(country, 'scale'))
 
         for crop in crops:
-            name_crop = 'cr' if use_cropland_mask else crop
+            for scale in scales:
+                all_combinations.extend(list(itertools.product([country], [crop], [scale])))
 
-        dir_crop_inputs = Path(f'crop_t{limit}') if threshold else Path(f'crop_p{limit}')
+    return all_combinations
 
-        # For each element in vars, create a list of files to be merged
-        frames = []
-        pb = tqdm(vars, leave=False)
-        for var in pb:
-            pb.set_description(f'Processing {var} for {country}')
-            pb.update()
 
-            path_var_files = params.dir_input / dir_crop_inputs / country / scale[0] / 'cr' / var
-            var_files = list(path_var_files.rglob('*.csv'))
+def merge_eo_files(params, country, crop, scale):
+    frames = []
+    cols = ['country', 'region', 'region_id', 'year', 'doy']
 
-            for fl in var_files:
-                frames.append(pd.read_csv(fl, usecols=cols + [var]))
+    use_cropland_mask = params.parser.get(country, 'use_cropland_mask')
+    threshold = params.parser.getboolean(country, 'threshold')
+    vars = ast.literal_eval(params.parser.get(country, 'eo_model'))
+    limit = common.crop_mask_limit(params, country, threshold)
 
-        result = None
-        for ix, df in tqdm(enumerate(frames), total=len(frames), desc='merging EO data'):
-            if ix >= 19:
-                breakpoint()
-            if result is None:
-                result = df.set_index(cols)
-            else:
-                result = result.combine_first(df.set_index(cols))
+    name_crop = 'cr' if use_cropland_mask else crop
+    dir_crop_inputs = Path(f'crop_t{limit}') if threshold else Path(f'crop_p{limit}')
 
+    # For each element in vars, create a list of files to be merged
+    for var in vars:
+        path_var_files = params.dir_input / dir_crop_inputs / country / scale / name_crop / var
+        var_files = list(path_var_files.rglob('*.csv'))
+
+        for fl in var_files:
+            frames.append(pd.read_csv(fl, usecols=cols + [var]))
+
+    # Merge files into dataframe
+    df_result = None
+    for df in tqdm(frames, total=len(frames), desc='merging EO data', leave=False):
+        if df_result is None:
+            df_result = df.set_index(cols)
+        else:
+            df_result = df_result.combine_first(df.set_index(cols))
+
+    return df_result
+
+
+def run(params):
+    num_cpus = int(params.fraction_cpus * cpu_count()) if params.parallel_process else 1
+
+    all_combinations = create_run_combinations(params)
+
+    pbar = tqdm(all_combinations)
+    for country, crop, scale in pbar:
+        # create dataframe for country, crop and scale (ccs)
+        df_ccs = merge_eo_files(params, country, crop, scale)
+
+        # Add crop calendar information
+
+        # Add yield, area and production information
 
         # Merge all csv files
         breakpoint()
